6a26540fed260acf7fe0da508dc4115a828c5d56
Zakaraie El Mejdki <elmejdki.zakariae@gmail.com>
2020-09-24 22:11:35 +0100

    Add third page for the second player form

--- assets/logic/app.js
+++ assets/logic/app.js
@@ -30,23 +30,36 @@ const gameBorad = (function () {
   const intiateButton = document.getElementById('game-initiator');
   const firstPage = document.querySelector('.first-page');
   const secondPage = document.querySelector('.second-page');
+  const thirdPage = document.querySelector('.third-page');
+
   const player1Symbols = document.getElementById('player1-symbols');
+  const player2Symbols = document.getElementById('player2-symbols');
+
   const player1Submit = document.getElementById('player1-submit-btn');
+  const player2Submit = document.getElementById('player2-submit-btn');
+
   const player1Name = document.getElementById('player1-name');
   const player1NameWarning = document.querySelector('.second-page .name-warning');
+  const player2NameWarning = document.querySelector('.third-page .name-warning');
+  const player2Name = document.getElementById('player2-name');
   let player1Symbol;
+  let player2Symbol;
   const player1SymbolWarning = document.querySelector('.second-page .symbol-warning');
+  const player2SymbolWarning = document.querySelector('.third-page .symbol-warning');
   // TODO: need to be removed
-  renderSymbols(player1Symbols);
+  // TODO: use settimeout to remove this item from the dom.
+  // player1Symbols.remove();
+  // renderSymbols(player2Symbols, 2);
 
   intiateButton.addEventListener('click', getPlayer1From);
   player1Submit.addEventListener('click', submitForm1);
+  player2Submit.addEventListener('click', submitForm2);
 
   function getPlayer1From() {
     firstPage.classList.add('hide-up');
     secondPage.classList.add('show-right');
 
-    // renderSymbols(player1Symbols);
+    renderSymbols(player1Symbols, 1);
   }
 
   function submitForm1(event) {
@@ -68,11 +81,43 @@ const gameBorad = (function () {
 
     if (!!player1Name.value && !!player1Symbol) {
       player1 = playerFactory(player1Name.value, player1Symbol.value);
+      symbols[Number(player1Symbol.getAttribute('data-id'))].selected = true;
       secondPage.classList.remove('show-right');
+      setTimeout(() => {
+        thirdPage.classList.add('show-right');
+        renderSymbols(player2Symbols, 2);
+      }, 2000);
     }
   }
 
-  function renderSymbols(parent) {
+  function submitForm2(event) {
+    event.preventDefault();
+
+    player2Symbol = document.querySelector(
+      ".third-page form input[type='radio']:checked",
+    );
+
+    if (player2Name.value === '') {
+      player2NameWarning.classList.add('active');
+      setTimeout(() => { player2NameWarning.classList.remove('active'); }, 4000);
+    }
+
+    if (player2Symbol === null) {
+      player2SymbolWarning.classList.add('active');
+      setTimeout(() => { player2SymbolWarning.classList.remove('active'); }, 4000);
+    }
+
+    if (!!player2Name.value && !!player2Symbol) {
+      player2 = playerFactory(player2Name.value, player2Symbol.value);
+      symbols[Number(player2Symbol.getAttribute('data-id'))].selected = true;
+      thirdPage.classList.remove('show-right');
+      setTimeout(() => {
+        // finalPage.classList.add('show-up');
+      }, 2000);
+    }
+  }
+
+  function renderSymbols(parent, name) {
     let radioContainer;
     let input;
     let label;
@@ -83,11 +128,16 @@ const gameBorad = (function () {
       label = document.createElement('label');
 
       radioContainer.className = 'radio-input';
-      input.id = `sym-${index}`;
+      input.id = `sym-${name}-${index}`;
+      input.setAttribute('data-id', index);
       input.setAttribute('type', 'radio');
-      input.setAttribute('name', 'symbols');
+      input.setAttribute('name', `symbols-${name}`);
       input.setAttribute('value', symbol.value);
-      label.setAttribute('for', `sym-${index}`);
+      if (symbol.selected) {
+        input.setAttribute('disabled', true);
+        label.classList.add('disabled');
+      }
+      label.setAttribute('for', `sym-${name}-${index}`);
       label.textContent = symbol.value;
       radioContainer.appendChild(input);
       radioContainer.appendChild(label);
